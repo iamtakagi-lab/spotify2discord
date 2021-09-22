507c6146e5ed574ef9bb7c7dbae8490e71463857
takagi <iam.takagi.dev@gmail.com>
2021-09-22 23:21:57 +0900

    Feat 曲を再生していないときの処理を追加

--- src/tasks/watch.ts
+++ src/tasks/watch.ts
@@ -4,58 +4,65 @@ import { store } from '..'
 import sendDiscord from '../discord'
 
 export default async () => {
-  if (
-    store.credential.accessToken == null ||
-    store.credential.refreshToken == null ||
-    store.credential.accessToken.length <= 0 ||
-    store.credential.refreshToken.length <= 0
-  )
-    return
+    if (
+        store.credential.accessToken == null ||
+        store.credential.refreshToken == null ||
+        store.credential.accessToken.length <= 0 ||
+        store.credential.refreshToken.length <= 0
+    )
+        return
 
-  const spotify = new SpotifyWebApi({
-    clientId: env.SPOTIFY_CLIENT_ID,
-    clientSecret: env.SPOTIFY_CLIENT_SECRET,
-    redirectUri: env.SPOTIFY_REDIRECT_URI,
-    accessToken: store.credential.accessToken,
-    refreshToken: store.credential.refreshToken,
-  })
+    const spotify = new SpotifyWebApi({
+        clientId: env.SPOTIFY_CLIENT_ID,
+        clientSecret: env.SPOTIFY_CLIENT_SECRET,
+        redirectUri: env.SPOTIFY_REDIRECT_URI,
+        accessToken: store.credential.accessToken,
+        refreshToken: store.credential.refreshToken,
+    })
 
-  spotify
-    .getMe()
-    .then((data) => {
-      return data.body
-    })
-    .catch(async (e) => {
-      const ref = (await spotify.refreshAccessToken()).body
-      const accessToken = ref.access_token
-      const refreshToken = ref.refresh_token
-      if (accessToken) {
-        store.setAccessToken(accessToken)
-        spotify.setAccessToken(accessToken)
-      }
-      if (refreshToken) {
-        store.setRefreshToken(refreshToken)
-        spotify.setRefreshToken(refreshToken)
-      }
-      const data = await spotify.getMe()
-      return data.body
-    })
-    .then(async (me) => {
-      const currentTrack = (await spotify.getMyCurrentPlayingTrack()).body
-      if (currentTrack && currentTrack.is_playing) {
-        //再生開始したとき
-        if (
-          store.track == null ||
-          store.track.item?.id != currentTrack.item?.id
-        ) {
-          const currentItem = currentTrack.item
-          if (currentItem) {
-            env.DISCORD_WEBOOK_URLS.split(',').map(async (url) => {
-              await sendDiscord(url.split('/'), spotify, currentTrack, me)
-            })
-            store.setTrack(currentTrack)
-          }
-        }
-      }
-    })
+    spotify
+        .getMe()
+        .then((data) => {
+            return data.body
+        })
+        .catch(async (e) => {
+            const ref = (await spotify.refreshAccessToken()).body
+            const accessToken = ref.access_token
+            const refreshToken = ref.refresh_token
+            if (accessToken) {
+                store.setAccessToken(accessToken)
+                spotify.setAccessToken(accessToken)
+            }
+            if (refreshToken) {
+                store.setRefreshToken(refreshToken)
+                spotify.setRefreshToken(refreshToken)
+            }
+            const data = await spotify.getMe()
+            return data.body
+        })
+        .then(async (me) => {
+            const currentTrack = (await spotify.getMyCurrentPlayingTrack()).body
+
+            //再生していないとき
+            if (store.track != null && currentTrack && !currentTrack.is_playing) {
+                store.resetTrack()
+                return
+            }
+
+            if (currentTrack && currentTrack.is_playing) {
+                //再生開始したとき
+                if (
+                    store.track == null ||
+                    store.track.item?.id != currentTrack.item?.id
+                ) {
+                    const currentItem = currentTrack.item
+                    if (currentItem) {
+                        env.DISCORD_WEBOOK_URLS.split(',').map(async (url) => {
+                            await sendDiscord(url.split('/'), spotify, currentTrack, me)
+                        })
+                        store.setTrack(currentTrack)
+                    }
+                }
+            }
+        })
 }
